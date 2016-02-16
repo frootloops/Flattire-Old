269f99641aa933a372f823cfc7c83e8a9711b8ba
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-02-17 00:19:41 +0300

    Add phone number validation

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,6 @@
 class Driver < ActiveRecord::Base
   devise :database_authenticatable, authentication_keys: [:phone]
   has_one_time_password counter_based: true
+  validates_uniqueness_of :phone
+  validates :phone, phone: true
 end
