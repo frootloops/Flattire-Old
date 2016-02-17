688de62824e5ca2f0c2257acdf94d8925ec11f1e
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-02-18 00:18:38 +0300

    Add specs for auth#hello

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::AuthController < ApiController
   def create
   end
 
-  def check
+  def hello
     render nothing: true
+  end
+
+  protected
+
+  def traveler_token_authenticable?
+    %w(hello).include?(action_name) ? true : super
   end
 end
---
+++ spec/requests/api/v1/auth_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Api::V1::AuthController, type: :request do
+  describe "GET #hello" do
+    it "returns 401 error" do
+      get '/api/v1/auth/hello', {}
+      expect(response).to have_http_status(:unauthorized)
+
+      headers = { "X-Driver-Token": "FAKE" }
+      get '/api/v1/auth/hello', {}, headers
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "returns 200 ok" do
+      driver = create(:driver)
+      headers = { "X-Driver-Token": driver.authentication_token }
+      get '/api/v1/auth/hello', {}, headers
+      expect(response).to be_success
+    end
+  end
+
+#   describe "POST #bonjour" do
+#     context "with new credential" do
+#       it "creates a traveler" do
+#         headers = { "X-Traveler-Device": "UUID" }
+#         post '/api/v1/bonjour', {}, headers
+#         expect(response).to have_http_status(:created)
+#         expect(Traveler.find_by(device_token: "UUID")).to be_kind_of(Traveler)
+#       end
+#     end
+#
+#     context "with an existing traveler" do
+#       it "regenerates a token" do
+#         traveler = create(:traveler)
+#         headers = { "X-Traveler-Device": traveler.device_token,
+#                     "X-Traveler-Token": "INVALID TOKEN" }
+#         expect do
+#           post '/api/v1/bonjour', {}, headers
+#           traveler.reload
+#         end.to change(traveler, :authentication_token)
+#         expect(response).to be_success
+#
+#         headers = { "X-Traveler-Device": traveler.device_token,
+#                     "X-Traveler-Token": traveler.authentication_token }
+#         get '/api/v1/bonjour/check', {}, headers
+#         expect(response).to be_success
+#       end
+#     end
+#   end
+end
